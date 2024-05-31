556566eedc66b7b74f690323e30088f296752097
triferlihandoyo123 <tri_ferli_handoyo@teknokrat.ac.id>
2024-06-01 01:20:14 +0700

    memodifikasi files json dan tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,11 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      'hero-pattern': "url('/img/hero-pattern.svg')",
+      'footer-texture': "url('/img/footer-texture.png')",
     },
   },
   plugins: [],
