0b4a60ca9e329c77a877f00fe9fcaa4aee3cf78e
triferlihandoyo123 <tri_ferli_handoyo@teknokrat.ac.id>
2024-06-01 01:46:50 +0700

    memodifikasi file Corsmiddlewarer

--- beckend/app/Http/Middleware/CorsMiddleware.php
+++ beckend/app/Http/Middleware/CorsMiddleware.php
@@ -5,21 +5,18 @@ use Closure;
 
 class CorsMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        
+
         $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        if ($request->getMethod() === "OPTIONS") {
+            return response()->json('OK', 200, $response->headers->all());
+        }
 
         return $response;
     }
