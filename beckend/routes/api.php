1bee89b2edbb240bcf8ab7751e40e18ea6fd3dcc
triferlihandoyo123 <tri_ferli_handoyo@teknokrat.ac.id>
2024-06-01 00:57:56 +0700

    menambahkan api untuk login register dan logout

--- beckend/routes/api.php
+++ beckend/routes/api.php
@@ -1,19 +1,17 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\IkanController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::post('/ikans', [IkanController::class, 'store']);
+Route::get('/ikans', [IkanController::class, 'index']);
+Route::get('/ikans/{id}', [IkanController::class, 'show']);
+Route::put('/ikans/{id}', [IkanController::class, 'update']);
+Route::delete('/ikans/{id}', [IkanController::class, 'destroy']);
+
