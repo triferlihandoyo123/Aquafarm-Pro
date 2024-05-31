60d83825a6bd03b81fe0d1346efcd00e6d785563
triferlihandoyo123 <tri_ferli_handoyo@teknokrat.ac.id>
2024-06-01 01:02:00 +0700

    memofikasi file web.php

--- beckend/routes/web.php
+++ beckend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
