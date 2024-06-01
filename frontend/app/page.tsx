2d70877970d702a2e42f1155158bf4c89efad49e
indahranifatmawati31@gmail.com <indahranifatmawati31@gmail.com>
2024-06-01 08:13:45 +0700

    memodifikasi file page global

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Login from "./login/login";
-
+import Dashboard from "./dashboardA/page";
 export default function Home() {
   return (
     <div className="bg-green-700">
